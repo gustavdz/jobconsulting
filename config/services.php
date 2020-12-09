1feb6f386497b14eb816472265bc358cf3266800
Erick <egordillo@redlinks.com.ec>
2020-12-09 16:59:45 -0500

    a

--- config/services.php
+++ config/services.php
@@ -30,4 +30,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),         // Your Facebook App Client ID
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'), // Your Facebook App Client Secret
+        'redirect' => env('FACEBOOK_REDIRECT'), // Your application route used to redirect users back to your app after authentication
+        'default_graph_version' => 'v2.12',
+    ],
+
 ];
