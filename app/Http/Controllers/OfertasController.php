9f09dc047cda69585560061f1063f627d3bef7bc
Gustavo Decker Zambrano <gustavdz@gmail.com>
2021-05-24 21:44:13 -0500

    order aspirantes-ofertas

--- app/Http/Controllers/OfertasController.php
+++ app/Http/Controllers/OfertasController.php
@@ -399,7 +399,8 @@ class OfertasController extends Controller
                                                             }])
                             ->with('oferta')
                             ->with('estado_oferta')
-                            ->where('oferta_id',$request->oferta_id);
+                            ->where('oferta_id',$request->oferta_id)
+                            ->orderBy('created_at','DESC');
                             if ($request->salario || $request->salario_max) {
                                 if (empty($request->salario_max)) {
                                     $aplicaciones = $aplicaciones->where('salario_aspirado','>=',$request->salario);
